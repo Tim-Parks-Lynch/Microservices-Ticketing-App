9922dcad48d45fa92fbc4fa16532aaf40667187e
Tim Janssen <tsplynch@gmail.com>
2022-08-09 20:27:51 -0400

    Added Custom Error Handling

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,16 +1,25 @@
 import express from 'express'
+import 'express-async-errors'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(express.json())
 
-app.use(signupRouter)
 app.use(currentUserRouter)
 app.use(signinRouter)
+app.use(signupRouter)
 app.use(signoutRouter)
+
+app.all('*', () => {
+	throw new NotFoundError()
+})
+
+app.use(errorHandler)
 
 app.listen(3000, () => {
 	console.log('listening on port 3000!!!!!')
